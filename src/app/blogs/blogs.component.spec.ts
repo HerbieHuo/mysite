51dd3365e07035b2020badc1be74c33b91532121
huo_hongbin <huo_hongbin@wuxiapptec.com>
2016-10-17 20:39:06 +0800

    添加blogs模块

---
+++ src/app/blogs/blogs.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { BlogsComponent } from './blogs.component';
+
+describe('Component: Blogs', () => {
+  it('should create an instance', () => {
+    let component = new BlogsComponent();
+    expect(component).toBeTruthy();
+  });
+});
